bb9737ab517b577cc1074d9274b01ab37b9ea765
Yuri CaBrera Reyes <ycr88@users.noreply.github.com>
2019-04-07 12:51:43 +0300

    Update index.js

--- front-end/test-social/index.js
+++ front-end/test-social/index.js
@@ -1,11 +1,16 @@
 var fbButton = document.getElementById('fb-share-button');
+var vkButton= document.getElementById('vk-share-button');
 var url =window.location.href;
 
 fbButton.addEventListener('click', function() {
-  console.log("que bola"+ fbButton.name);
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+      window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
         'facebook-share-dialog',
         'width=600,height=400'
     );
+  vkButton.addEventListener('load', function() {
+    var link= $this.getElementBytag('a');
+    console.log(link);
+    }   
     return false;
 });
+
